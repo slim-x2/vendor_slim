497962f27ebe265c00ec3822b203452c15e11a81
Christian Oder <myself5@carbonrom.org>
2017-09-29 22:48:51 +0200

    vendor: use SDClang 4.0
    Change-Id: I3cafca38b0684078fe88365afeb18fc5d3c9fe33

--- config/common.mk
+++ config/common.mk
@@ -203,8 +203,7 @@ endif
 
 # Include SDCLANG definitions if it is requested and available
 ifeq ($(HOST_OS),linux)
-    ifneq ($(wildcard vendor/qcom/sdclang-3.8/),)
+    ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
         include vendor/slim/sdclang/sdclang.mk
     endif
 endif
-
